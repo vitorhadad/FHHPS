12c874c28f0fe8e265aa222a9b8d5184ad16f579
vitorhadad <hadad.vitor@bc.edu>
2015-10-31 12:00:25 -0400

    Working char fct estimation. Added kreg to denominator as well

--- cpp_functions.cpp
+++ cpp_functions.cpp
@@ -56,16 +56,30 @@ cx_mat numerator_var_creator(mat Y1,
 }
 
 
+// denominator var[i] = exp(i * s * G3(x[i]) * W[i])
+cx_mat denominator_var_creator(mat W1,
+			   		         rowvec x1,
+			   		         mat s1)  {
 
 
-// Characteristic function of G1^{-1}(x)'Y 
-cx_colvec cf_numerator(mat Y1,
-			           mat X1,
+	mat tmp = s1.t() * g3(x1) * W1.t();
+
+    // Using Euler's formula: exp(iz) = cos(z) + isin(z) 
+    cx_mat denominator_var = cx_mat(arma::cos(tmp), arma::sin(tmp));
+	return denominator_var.t();
+}
+
+
+
+
+// Characteristic function of numerator 
+cx_colvec cf_numerator(mat y1,
+			           mat x1,
 			           mat s1,
 			           double b1) {
 
-	int n_obs = X1.n_rows;
-	int n_features = X1.n_cols;
+	int n_obs = x1.n_rows;
+	int n_features = x1.n_cols;
 
 	mat x = mat(1, n_features);
 	cx_mat numerator_var = cx_mat(n_obs,1);
@@ -73,16 +87,15 @@ cx_colvec cf_numerator(mat Y1,
 
 
 	for (int i = 0; i < n_obs; i++) {
-		x = X1.row(i);
-		numerator_var = numerator_var_creator(Y1, x, s1);
-		results(i) = kreg(numerator_var, X1, x, b1);
+		x = x1.row(i);
+		numerator_var = numerator_var_creator(y1, x, s1);
+		results(i) = kreg(numerator_var, x1, x, b1);
 	}
-
 	return results;
 }
 
 
-// Increases the modulus to lower_bound
+// increases the modulus to lower_bound
 cx_colvec trim(cx_colvec v, double lower_bound) {
 
     for (int i = 0; i < v.n_elem; i++) {
@@ -91,28 +104,26 @@ cx_colvec trim(cx_colvec v, double lower_bound) {
         }
     }
     return v;
-}
-
-    
+} 
 
 
-// Characteristic function of denominator variable
-// denominator variable = i * s * G_3(x) * W
+// Characteristic fct of denominator
 cx_colvec cf_denominator(mat W1,
-			   		    mat X1,
-			   		    mat s1) {
+			           mat X1,
+			           mat s1,
+			           double b1) {
 
 	int n_obs = X1.n_rows;
+	int n_features = X1.n_cols;
+
+	mat x = mat(1, n_features);
+	cx_mat denominator_var = cx_mat(n_obs,1);
 	cx_colvec results = cx_colvec(n_obs);
-	cx_mat tmp = cx_mat(1, n_obs);
-	rowvec x = rowvec(2);
 
 	for (int i = 0; i < n_obs; i++) {
 		x = X1.row(i);
-	    // Euler's formula
-        tmp.set_real(arma::cos(s1.t() * g3(x) * W1.t()));
-		tmp.set_imag(arma::sin(s1.t() * g3(x) * W1.t()));
-		results(i) = mean(mean(tmp, 1));
+		denominator_var = denominator_var_creator(W1, x, s1);
+		results(i) = kreg(denominator_var, X1, x, b1);
 	}
 	return results;
 }
@@ -138,13 +149,13 @@ ComplexVector target_cf(NumericMatrix Y,
     int s_pts = s_grid1.n_elem;
     mat s = zeros(2,1);
 
-    cx_vec target_cf = cx_vec(s_pts*s_pts);
+    cx_mat target_cf = cx_mat(s_pts, s_pts);
     
     int k = 0;
     for (int i = 0; i < s_pts; i++) {
         for (int j = 0; j < s_pts; j++) {
             s(0) = s_grid1(i); s(1) = s_grid1(j);
-            target_cf[k++] = arma::mean(arma::mean(cf_numerator(Y1, X1, s, b1)/cf_denominator(W1, X1, s)));
+            target_cf(i,j) = arma::mean(arma::mean(cf_numerator(Y1, X1, s, b1)/cf_denominator(W1, X1, s, b1)));
         }
     }  
 
@@ -168,43 +179,9 @@ ComplexVector rhs_variable(NumericMatrix Y,
 
     // With denominator trimming 
     cx_colvec numerator = cf_numerator(Y1, X1, s1, b1);
-	cx_colvec denominator = trim(cf_denominator(W1, X1, s1), .01);
+	cx_colvec denominator = trim(cf_denominator(W1, X1, s1, b1), .01);
 
 	return wrap(arma::mean(numerator/denominator));
-}
-
-
-
-// This is just a wrapper to call function from R
-// [[Rcpp::export]]
-ComplexVector cf_numerator_R(NumericMatrix Y,
-        				     NumericMatrix X,
-        					 NumericMatrix s,
-                             double b1)  {
-
-	mat Y1 = as<mat>(Y);
-	mat X1 = as<mat>(X);    
-	mat s1 = as<mat>(s);
-
-	cx_colvec output = cf_numerator(Y1, X1, s1, b1);
-
-  return wrap(output);
-}
-
-
-// This is just a wrapper to call function from R
-// [[Rcpp::export]]
-ComplexVector cf_denominator_R(NumericMatrix W,
-                               NumericMatrix X,
-        					   NumericMatrix s)  {
-
-	mat W1 = as<mat>(W);
-	mat X1 = as<mat>(X);    
-	mat s1 = as<mat>(s);
-
-	cx_colvec output = trim(cf_denominator(W1, X1, s1), 0.01);
-
-  return wrap(output);
 }
 
 
