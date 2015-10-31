ae59baa6a47c5620d2968f2580ab19c84c8e3e1c
vitorhadad <hadad.vitor@bc.edu>
2015-10-31 11:27:07 -0400

    WORKING

--- minimal_implementation.r
+++ minimal_implementation.r
@@ -4,14 +4,14 @@ source("functions.R")
 sourceCpp("cpp_functions.cpp")
 
 # Data
-n_obs =  2500
+n_obs =  10000
 ds <- create_data(n_obs)
 Y <- matrix(ds$Y, n_obs, 2)
 X <- matrix(ds$X, n_obs, 2)
 W <- matrix(ds$W, n_obs, 2)
 
 # Rule out small determinants
-lower_bound = .1
+lower_bound = 1 
 valid <- sapply(seq(n_obs), function(i) (det(g1(X[i,])) > lower_bound))
 Y <- Y[valid,]
 X <- X[valid,]
@@ -24,14 +24,15 @@ numerator_regression <- function(s) apply(X, 1,
                              function(x) kreg_R(Y = numerator_variable(s, x),
                                                 X = X,
                                                 matrix(x, 1, 2),
-                                                b = diff(range(X))*.3))
+                                                b = diff(range(X))*.01))
 # Denominator
 denominator_variable <- function(s, x) t(exp(1i * t(s) %*% g3(x) %*% t(W)))
+trim <- function(z) return(if (abs(z) < .1) { .1*z/abs(z) } else {z}) 
 denominator_regression <- function(s) apply(X, 1,
-                             function(x) kreg_R(Y = denominator_variable(s, x),
+                             function(x) trim(kreg_R(Y = denominator_variable(s, x),
                                                 X = X,
                                                 matrix(x, 1, 2),
-                                                b = diff(range(X))*.1))
+                                                b = diff(range(X))*.01)))
 # Mean of ratio
 ratio <- function(s) mean(numerator_regression(s) / denominator_regression(s))
 
@@ -39,12 +40,13 @@ ratio <- function(s) mean(numerator_regression(s) / denominator_regression(s))
 # Normal cf (estimation target)
 mean_AB <- matrix(c(mean(ds$A), mean(ds$B)), 2, 1)
 cov_AB <- cov(cbind(ds$A, ds$B))
-normal_cf <- function(s) mean(exp(1i * t(s) %*% mean_AB - .5 * t(s) %*% cov_AB %*% s))
+normal_cf <- function(s) mean(exp(1i * t(s) %*% mean_AB 
+                                  - .5 * t(s) %*% cov_AB %*% s))
 
 
 
 # Evaluate over grid
-n_pts <- 8
+n_pts <- 13
 s_grid <- seq(-.5, .5, length = n_pts)
 estimated <- matrix(0, n_pts, n_pts)
 target <- matrix(0, n_pts, n_pts)
@@ -62,6 +64,8 @@ for (i in seq(n_pts)) {
     }
 }
 
+
+dev.new()
 par(mfrow = c(2,2))
 persp(s_grid, s_grid, abs(estimated), theta = 45, phi = 45, main = "estimated")
 persp(s_grid, s_grid, abs(target), theta = 45, phi = 45, main = "target")
@@ -69,5 +73,16 @@ persp(s_grid, s_grid, abs(numerator), theta = 45, phi = 45, main = "mean(numerat
 persp(s_grid, s_grid, abs(denominator), theta = 45, phi = 45, main = "mean(denominator)")
 
 save.image("minimal_implementation.RData")
+
+dev.new()
+par(mfrow = c(2,2))
+contour(s_grid, s_grid, abs(estimated), main = "estimated")
+contour(s_grid, s_grid, abs(target), main = "target")
+contour(s_grid, s_grid, abs(numerator), main = "mean(numerator)")
+contour(s_grid, s_grid, abs(denominator), main = "mean(denominator)")
+
+save.image("minimal_implementation.RData")
+
+
 
 
